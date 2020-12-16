7d34c6f93995c46610ba723e83d11933a09ae724
gojnimer <gojnimer@gmail.com>
2020-12-15 21:11:57 -0300

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  defaultauth: 'firebase',
+  firebaseConfig: {
+    apiKey: "AIzaSyBBkzhNZVVIaPrKorw373W_CYlEBKZSsK0",
+    authDomain: "chat-angular-a0a4e.firebaseapp.com",
+    databaseURL: "https://chat-angular-a0a4e.firebaseio.com",
+    projectId: "chat-angular-a0a4e",
+    storageBucket: "chat-angular-a0a4e.appspot.com",
+    messagingSenderId: "540533447011",
+    appId: "1:540533447011:web:84cab0636ed627393cdb56",
+    measurementId: "G-3JJKXYWK7C"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,37 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  defaultauth: 'firebase',
+  firebaseConfig: {
+    apiKey: "AIzaSyBBkzhNZVVIaPrKorw373W_CYlEBKZSsK0",
+    authDomain: "chat-angular-a0a4e.firebaseapp.com",
+    databaseURL: "https://chat-angular-a0a4e.firebaseio.com",
+    projectId: "chat-angular-a0a4e",
+    storageBucket: "chat-angular-a0a4e.appspot.com",
+    messagingSenderId: "540533447011",
+    appId: "1:540533447011:web:84cab0636ed627393cdb56",
+    measurementId: "G-3JJKXYWK7C"
+  }
+  /* firebaseConfig: {
+    apiKey: 'AIzaSyBBkzhNZVVIaPrKorw373W_CYlEBKZSsK0',
+    authDomain: 'https://chat-angular-a0a4e.firebaseapp.com/',
+    databaseURL: '',
+    projectId: 'chat-angular-a0a4e',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '1:540533447011:web:84cab0636ed627393cdb56',
+    measurementId: ''
+  } */
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
